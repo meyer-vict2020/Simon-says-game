23010c6099dd5d27415527a057e3306d74c68b34
Tori Meyer <mey20010@byui.edu>
2022-07-12 16:42:22 -0600

    Add files

---
+++ SimonSays/Program.cs
@@ -0,0 +1,68 @@
+﻿using Byui.Games.Casting;
+using Byui.Games.Directing;
+using Byui.Games.Scripting;
+using Byui.Games.Services;
+
+
+namespace Example.Scaling
+{
+    /// <summary>
+    /// The entry point for the program.
+    /// </summary>
+    /// <remarks>
+    /// The purpose of this program is to demonstrate how Actors, Actions, Services and a Director 
+    /// work together to scale an actor up and down on the screen.
+    /// </remarks>
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Instantiate a service factory for other objects to use.
+            IServiceFactory serviceFactory = new RaylibServiceFactory();
+
+            // Instantiate the actors that are used in this example.
+            Label label = new Label();
+            label.Display("Simon Says");
+            label.MoveTo(25, 25);
+            
+            Actor square1 = new Actor();
+            square1.SizeTo(100, 100);
+            square1.MoveTo(200, 100);
+            square1.Tint(Color.Blue());
+
+            Actor square2 = new Actor();
+            square2.SizeTo(100, 100);
+            square2.MoveTo(340, 100);
+            square2.Tint(Color.Red());
+
+            Actor square3 = new Actor();
+            square3.SizeTo(100, 100);
+            square3.MoveTo(200, 250);
+            square3.Tint(Color.Yellow());
+
+            Actor square4 = new Actor();
+            square4.SizeTo(100, 100);
+            square4.MoveTo(340, 250);
+            square4.Tint(Color.Green());
+
+            // Instantiate the actions that use the actors.
+            ScaleActorAction scaleActorAction = new ScaleActorAction(serviceFactory);
+            DrawActorAction drawActorAction = new DrawActorAction(serviceFactory);
+
+            // Instantiate a new scene, add the actors and actions.
+            Scene scene = new Scene();
+            scene.AddActor("actors", square1);
+            scene.AddActor("actors", square2);
+            scene.AddActor("actors", square3);
+            scene.AddActor("actors", square4);
+
+            scene.AddActor("labels", label);
+            scene.AddAction(Phase.Input, scaleActorAction);
+            scene.AddAction(Phase.Output, drawActorAction);
+
+            // Start the game.
+            Director director = new Director(serviceFactory);
+            director.Direct(scene);
+        }
+    }
+}
---
+++ SimonSays/ScaleActorAction.cs
@@ -0,0 +1,51 @@
+using System;
+using Byui.Games.Casting;
+using Byui.Games.Scripting;
+using Byui.Games.Services;
+
+
+namespace Example.Scaling
+{
+    /// <summary>
+    /// Scales the actor up or down depending on key presses.
+    /// </summary>
+    public class ScaleActorAction : Byui.Games.Scripting.Action
+    {
+        private IKeyboardService _keyboardService;
+        private IMouseService _mouseService;
+
+        public ScaleActorAction(IServiceFactory serviceFactory)
+        {
+            _keyboardService = serviceFactory.GetKeyboardService();
+            _mouseService = serviceFactory.GetMouseService();
+        }
+
+        public override void Execute(Scene scene, float deltaTime, IActionCallback callback)
+        {
+            try
+            {
+                // get the actor from the cast
+                List<Actor> squares = scene.GetAllActors("actors");
+
+                foreach(Actor square in squares)
+                {
+                    // scale the actor up or down 
+                    if (_mouseService.IsButtonPressed(MouseButton.Left))
+                    {
+                        // scale the actor up a maximum of 300 percent
+                        float percent1 = (square.GetScale() < 3.0) ? 0.025f : 0;
+                        square.Scale(percent1);
+                        // scale the actor down to a minimum of 30 percent
+                        float percent2 = (square.GetScale() > 0.3) ? -0.025f : 0;
+                        square.Scale(percent2);
+                    }
+                }
+                
+            }
+            catch (Exception exception)
+            {
+                callback.OnError("Couldn't scale actor.", exception);
+            }
+        }
+    }
+}
